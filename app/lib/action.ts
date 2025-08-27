7d3cdb2a22e887807f98d3872eb7bb9410c77a55
jojo_yau <jojo_yau@JOJOYAU-PCW10B>
2025-08-27 17:35:48 +0800

    invoice create: adding form validation

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -10,16 +10,49 @@ const sql = neon(process.env.POSTGRES_URL!);
 
 const FormSchema = z.object({
   id: z.string(),
-  amount: z.coerce.number(),
-  customerId: z.string(),
-  status: z.enum(["pending", "paid"]),
+
+  ////alt 1: <=0 and "" will output the same error, which is "gt", only NaN will be solely picked out
+  // amount: z.coerce.number({invalid_type_error:'needs to be \'number\' '}).gt(0,"that means you didn't fill out a number or the number is smaller than or equal to 0"),
+
+//// alt 2: It can distinguish "" from <= 0, and also "" from NaN
+// Warning: `.coerce` is not a schema. It's a ZodEffects wrapper that applies a data effect,
+// in this case essentially `Number(...)` without validation. If you use `coerce`,
+// `undefined` will get converted to `NaN` and then passed into the inner `number()` schema,
+// which triggers `invalid_type_error` instead of `required_error`—not what we want here. 
+  amount: z.preprocess(v=>v===''?undefined:Number(v), z.number({
+    invalid_type_error:'要是數字',
+    required_error:'要填'
+  }).gt(0,"要大於零")),
+
+  //alt 3: It can distinguish "", NaN, and <=0, which is the most detailed
+  // amount: z
+  //   .string()
+  //   .min(1, "at least oneword")
+  //   .transform((v) => Number(v))
+  //   .pipe(
+  //     z
+  //       .number({ invalid_type_error: "please filled out a number" })
+  //       .gt(0, "needs to > 0")
+  //   ),
+
+  customerId: z.string({ required_error: "no id" }),
+  status: z.enum(["pending", "paid"], { required_error: "要填阿幹嘛?" }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formdata: FormData) {
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message: string | null;
+};
+
+export async function createInvoice(formState: State, formdata: FormData) {
   //   const rawFormData = {
   //     amount: formdata.get('amount'),
   //     customerId: formdata.get('customerId'),
@@ -34,21 +67,41 @@ export async function createInvoice(formdata: FormData) {
 
   // const rawFormData = Object.fromEntries(formdata)
 
-  const { customerId, amount, status } = CreateInvoice.parse(
-    Object.fromEntries(formdata)
-  );
+  const validation = CreateInvoice.safeParse(Object.fromEntries(formdata));
+
+  if (!validation.success) {
+    // console.log(validation.error?.flatten().fieldErrors);
+
+    const newFormState = { ...formState };
+
+    newFormState.errors = validation.error?.flatten().fieldErrors;
+    newFormState.message = "invoice creating failed";
+
+    // console.log(formState)
+
+    return newFormState;
+  }
+
+  const { customerId, amount, status } = validation.data;
+
+  // const { customerId, amount, status } = CreateInvoice.parse(
+  //   Object.fromEntries(formdata)
+  // );
 
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
   try {
+
+    // throw new Error();
+
     await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-  } catch(e) {
-
-    console.log(e)
-
+  } catch (e) {
+    return {
+      message: "Database Error: Failed to Create Invoice.",
+    };
   }
 
   revalidatePath("/dashboard/invoices");
@@ -72,9 +125,7 @@ export async function updateInvoice(id: string, formdata: FormData) {
      where id = ${id}
   `;
   } catch (error) {
-
-    console.log(error)
-
+    console.log(error);
   }
 
   revalidatePath("/dashboard/invoices");
@@ -82,16 +133,12 @@ export async function updateInvoice(id: string, formdata: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
+  throw new Error(":O");
 
-   
-
-    throw new Error(":O")
-
-    await sql`  
+  await sql`  
     delete from invoices where id = ${id}
   
   `;
-       
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,17 +1,25 @@
-import { CustomerField } from '@/app/lib/definitions';
-import Link from 'next/link';
+"use client";
+
+import { CustomerField } from "@/app/lib/definitions";
+import Link from "next/link";
 import {
   CheckIcon,
   ClockIcon,
   CurrencyDollarIcon,
   UserCircleIcon,
-} from '@heroicons/react/24/outline';
-import { Button } from '@/app/ui/button';
-import { createInvoice } from '@/app/lib/action';
+} from "@heroicons/react/24/outline";
+import { Button } from "@/app/ui/button";
+import { createInvoice, State } from "@/app/lib/action";
+import { useActionState } from "react";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const initialState: State = { errors: {}, message: null };
+  const [state, formState] = useActionState(createInvoice, initialState);
+
+  console.log(state);
+
   return (
-    <form action={createInvoice}>
+    <form action={formState}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -36,6 +44,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.customerId &&
+              state.errors.customerId.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
         </div>
 
         {/* Invoice Amount */}
@@ -48,13 +64,21 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               <input
                 id="amount"
                 name="amount"
-                type="number"
+                type=""
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+          </div>
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.amount &&
+              state.errors.amount.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
           </div>
         </div>
 
@@ -98,7 +122,15 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             </div>
           </div>
         </fieldset>
+        {state.errors?.status &&
+          state.errors.status.map((error: string) => (
+            <p className="mt-2 text-sm text-red-500" key={error}>
+              {error}
+            </p>
+          ))}
+        <p className="mt-2 text-sm text-red-500">{state.message}</p>
       </div>
+
       <div className="mt-6 flex justify-end gap-4">
         <Link
           href="/dashboard/invoices"
@@ -106,6 +138,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
         >
           Cancel
         </Link>
+
         <Button type="submit">Create Invoice</Button>
       </div>
     </form>
